43e17c80257ae680cf3bdd98423fc24b042718e4
josepaniagua <jpanjr@gmail.com>
2019-08-27 22:23:19 -0700

    added sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import "./App.css";
+import "./Sass/App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
     </div>
   );
 }
