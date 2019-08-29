7612107ddd20ebfa02e63a1f4d74b3b89561c536
josepaniagua <jpanjr@gmail.com>
2019-08-29 16:16:03 -0700

    api is functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Sass/App.css";
 import NavBar from "./Components/Navigation/NavBar";
 import Footer from "./Components/Footer/Footer";
+// import Pics from "./Components/MainBody/Pics";
+import axios from 'axios';
+
+
 
 function App() {
+    const [nasaImages, setNasaImages] = useState([]);
+    var _0x1b91=["\x64\x49\x4D\x66\x44\x71\x6D\x33\x65\x7A\x4A\x4C\x48\x75\x37\x43\x71\x6B\x70\x79\x39\x36\x54\x30\x78\x4E\x4B\x70\x4F\x79\x79\x47\x53\x58\x44\x79\x4E\x65\x78\x76"];const llave_de_ipa=_0x1b91[0]
+
+
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${llave_de_ipa}`)
+            .then(res => {
+                console.log(res);
+                setNasaImages(res.data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
   return (
     <div className="App">
       <NavBar/>
-      <div></div>
+      <div>
+          {nasaImages.copyright}
+          {/*<Pics pic={eachPic} />*/}
+      </div>
     <Footer/>
     </div>
   );
--- src/Components/MainBody/Pics.js
+++ src/Components/MainBody/Pics.js
@@ -1,8 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import '../../Sass/App.css';
-
-const Pics = () => (
-    <div></div>
-);
-
-export default Pics;
+// import React from 'react';
+// import '../../Sass/App.css';
+//
+// const Pics = (props) => {
+//     const { pic } = props;
+//     return (
+//         <div>
+//             {pic.map((nasa, index) => (
+//                 <img key={index} src={nasa} alt="random pic of space" />
+//             )) }
+//         </div>
+//     );
+// };
+//
+// export default Pics;
